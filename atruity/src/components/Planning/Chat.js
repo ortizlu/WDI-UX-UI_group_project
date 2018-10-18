6f9f1d90ed03d5b887e12758f6d0c579b541238b
Luis Ortiz <ortizlu@icloud.com>
2018-10-18 11:28:02 -0400

    add chat box

---
+++ atruity/src/components/Planning/Chat.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './Chat.css'
+
+class Chat extends Component {
+  render() {
+    return (
+      <div className="chat">
+        <div>
+          <h3>Live Chat</h3>
+        </div>
+        <div>
+          <p>Michael has just joined the chat room</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Chat
